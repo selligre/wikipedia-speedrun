3a3aa6101ac8988c8aa1201bae661d8e835162e6
selligre <92374805+selligre@users.noreply.github.com>
2023-01-27 12:38:01 +0100

    avancement question 1

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from pip._vendor import requests
+from bs4 import BeautifulSoup
+import re
 
-print("start")
-
-base = "https://iceandfire.fandom.com/wiki/"
-source = ""
-cible = ""
+base = 'https://iceandfire.fandom.com/wiki/'
+source = ''
+cible = ''
 
 
 def main(source, cible):
-    print("source: ", source)
-    print("cible: ", cible)
+    """doc main"""
+    print('source: ', source)
+    print('cible: ', cible)
+
 
 def liste_liens(page):
-    print(requests.get(base + page))
+    """doc liste_liens"""
+    content = requests.get(base + page)
+    print(base + page)
+    # dans la page
+    soup = BeautifulSoup(content.text, 'html.parser')
+    # dans le body
 
-main("House_Lannister", "House_Stark")
-liste_liens("House_Lannister")
-print("end")
-print("dernier test")
+    # contient /wiki/
+    soup = soup.find_all(href=re.compile('/wiki/'))
+    print(len(soup))
+    liste = []
+    for link in soup:
+        liste.append(link.get('href'))
+    # enlever les doubles
+    liste = list(set(liste))
+    if (liste == []): return None
+    return liste
+
+
+# main('House_Lannister', 'House_Stark')
+print(liste_liens('House_Stark'))

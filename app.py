2b813a9d44cf344d3db945c23aa79a43e8bdb5b0
selligre <selligre@gmail.com>
2023-01-27 17:32:01 +0100

    avancement question 1

--- app.py
+++ app.py
@@ -14,28 +14,49 @@ def main(source, cible):
 
 
 def liste_liens(page):
-    """doc liste_liens"""
+    """doc liste_liens(page)"""
     content = requests.get(base + page)
     print(base + page)
-    # select page
-    soup = BeautifulSoup(content.text, 'html.parser')
-    # select body
-    soup = soup.find('body')
+    # CONTENT SELECTION
+    # select body in page
+    soup = BeautifulSoup(content.text, 'html.parser').find('body')
     # that contains /wiki/
     soup = soup.find_all(href=re.compile('/wiki/'))
-    print(len(soup))
-    liste = []
+    # LIST REDUCTION
+    # get list from href
+    l = []
     for link in soup:
-        liste.append(link.get('href'))
-    # remove extra caracters in urls
-    for link in liste:
-        if (link.startswith('https:')):
-            link.replace('https:', '')
+        l.append(link.get('href'))
+    print('len(l) with href ' + str(len(l)))
+    # remove 'https://iceandfire.fandom.com'
+    # for element in str(l):
+    #     if 'https://iceandfire.fandom.com' in element:
+    #         element.replace('https://iceandfire.fandom.com', '')
+    # print('len(l) without https ' + str(len(l)))
+    # remove elements from another website
+    # for element in l:
+    #     if 'https' in element:
+    #         l.remove(element)
+    # print('len(l) without foreign pages ' + str(len(l)))
+    # remove elements with ':'
+    print(l)
+    l = [element for element in l if ':' in element]
+    print('len(l) without ":" ' + str(len(l)))
+    print(l)
+    # remove '/wiki/'
+    # for element in l:
+    #     if '/wiki/' in element:
+    #         element.replace('/wiki/', '')
+    # print('len(l) without "/wiki/" ' + str(len(l)))
     # remove doubles
-    liste = list(set(liste))
-    if (liste == []): return None
-    return liste
+    # l = list(set(l))
+    # print('len(l) without doubles ' + str(len(l)))
+    # RETURN
+    # if empty list, then there is no path, we must return None
+    if (l == []): return None
+    # else, return the list
+    return l
 
 
 # main('House_Lannister', 'House_Stark')
-print(liste_liens('House_Stark'))
+liste_liens('Petyr_Baelish')

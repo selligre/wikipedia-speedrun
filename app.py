13bffab7a726a00cd1159678e00fc18924294964
selligre <selligre@gmail.com>
2023-01-28 14:22:35 +0100

    question 1 presque finie

--- app.py
+++ app.py
@@ -24,38 +24,55 @@ def liste_liens(page):
     soup = soup.find_all(href=re.compile('/wiki/'))
     # LIST REDUCTION
     # get list from href
-    l = []
+    liste = []
     for link in soup:
-        l.append(link.get('href'))
-    print('len(l) with href ' + str(len(l)))
+        liste.append(link.get('href'))
+    print('len(l) with href ' + str(len(liste)))
+    print(liste)
     # remove 'https://iceandfire.fandom.com'
-    # for element in str(l):
-    #     if 'https://iceandfire.fandom.com' in element:
-    #         element.replace('https://iceandfire.fandom.com', '')
-    # print('len(l) without https ' + str(len(l)))
+    for element in range(len(liste)):
+        if 'https://iceandfire.fandom.com' in liste[element]:
+            liste[element] = liste[element].replace('https://iceandfire.fandom.com', '')
+    print('len(l) without https ' + str(len(liste)))
+    print(liste)
     # remove elements from another website
-    # for element in l:
-    #     if 'https' in element:
-    #         l.remove(element)
-    # print('len(l) without foreign pages ' + str(len(l)))
-    # remove elements with ':'
-    print(l)
-    l = [element for element in l if ':' in element]
-    print('len(l) without ":" ' + str(len(l)))
-    print(l)
+    for element in range(len(liste) - 1):
+        if 'https' in liste[len(liste) - element - 1]:
+            liste.pop(element)
+    print('len(l) without foreign pages ' + str(len(liste)))
+    print(liste)
+    # remove elements with ':' REFAIRE AVEC LA BOUCLE POP
+    liste = [element for element in liste if ':' not in element]
+    print('len(l) without ":" ' + str(len(liste)))
+    print(liste)
+    # remove elements with 'POV' REFAIRE AVEC LA BOUCLE POP
+    liste = [element for element in liste if 'POV' not in element]
+    print('len(l) without "POV" ' + str(len(liste)))
+    print(liste)
+    # remove elements with '=' REFAIRE AVEC LA BOUCLE POP
+    liste = [element for element in liste if '=' not in element]
+    print('len(l) without "=" ' + str(len(liste)))
+    print(liste)
+    # remove elements with 'community'
+    liste = [element for element in liste if 'community' not in element]
+    print('len(l) without "community" ' + str(len(liste)))
+    print(liste)
     # remove '/wiki/'
-    # for element in l:
-    #     if '/wiki/' in element:
-    #         element.replace('/wiki/', '')
-    # print('len(l) without "/wiki/" ' + str(len(l)))
+    for element in range(len(liste)):
+        if '/wiki/' in liste[element]:
+            liste[element] = liste[element].replace('/wiki/', '')
+    print('len(l) without "/wiki/" ' + str(len(liste)))
+    print(liste)
     # remove doubles
-    # l = list(set(l))
-    # print('len(l) without doubles ' + str(len(l)))
+    liste = list(set(liste))
+    print('len(l) without doubles ' + str(len(liste)))
+    print(liste)
     # RETURN
     # if empty list, then there is no path, we must return None
-    if (l == []): return None
+    if not liste:
+        return None
     # else, return the list
-    return l
+    return liste
 
 
 # main('House_Lannister', 'House_Stark')

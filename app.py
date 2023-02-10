bdf9cd16f8c16d03751277e53c9c9b17c3dce20c
selligre <92374805+selligre@users.noreply.github.com>
2023-02-10 10:18:00 +0100

    debut question 5

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from pip._vendor import requests
 from bs4 import BeautifulSoup
 import json
 
+BASE = 'https://iceandfire.fandom.com/wiki/'
+
 
 def main(source_str, cible_str):
     """doc main"""
@@ -11,7 +13,7 @@ def main(source_str, cible_str):
 
 def liste_liens(page):
     """doc liste_liens(page)"""
-    content = requests.get(base + page)
+    content = requests.get(BASE + page)
     # CONTENT SELECTION
     soup = BeautifulSoup(content.content, 'html.parser')
     soup = soup.select('div.mw-parser-output a[href^="/wiki/"][title]')
@@ -28,8 +30,8 @@ def liste_liens(page):
     # remove doubles
     liste = list(set(liste))
     # RETURN
-    if not liste:
-        return None
+    # if not liste:
+    #     return None
     return liste
 
 
@@ -74,6 +76,21 @@ def build_wiki_from(d, start_page):
     return d
 
 
+def build_wiki_from_largeur(wiki, source):
+    f = []
+    fait = []
+    f.append(source)
+    fait.append(source)
+    while f:
+        print(len(wiki.keys()))
+        source = f.pop(0)
+        wiki.update({source: liste_liens(source)})
+        for voisin in wiki[source]:
+            if voisin not in fait:
+                f.append(voisin)
+                fait.append(voisin)
+
+
 def is_wiki_finished(dico):
     result = True
     for key in dico.copy().keys():
@@ -84,12 +101,34 @@ def is_wiki_finished(dico):
     return result
 
 
-base = 'https://iceandfire.fandom.com/wiki/'
-source = 'Petyr_Baelish'
-cible = ''
-wiki = {}
-svg_dico_json(build_wiki_from(wiki, source), 'fichier_cible.json')
+# wiki = {}
+# svg_dico_json(build_wiki_from_largeur(wiki, source), 'fichier_cible.json')
 
 # les url Joffrey_I_Baratheon, Joffrey et Joffrey_Baratheon conduisent a la meme page, donc donnent la meme liste
 # 2658 pages dans le dico contre 2534 pages dans le wiki, donc 124 urls conduisent a des pages identiques
-# temps d'exec pour build le wiki depuis 'Petyr_Baelish': 5min7sec
+# temps d'exec pour build le wiki depuis 'Petyr_Baelish': 5min7sec, 2658 pages
+# temps d'exec pour build_largeur depuis 'Petyr_Baelish': 9min50sec, 2667 pages
+
+def plus_court_chemin(page_source, page_cible):
+    # result list
+    result = []
+    result.append(page_source)
+    # wiki to search
+    wiki = build_wiki_from({}, page_source)
+    for key in wiki.keys():
+        for value in wiki[key]:
+            if value == page_cible:
+                print(value)
+                print(key)
+                result.append(key)
+                break
+    result.append(page_cible)
+    # RETURN
+    if not result[1]:
+        return None
+    return result
+
+
+source = 'Dorne'
+cible = 'Rhaego'
+print(plus_court_chemin(source, cible))

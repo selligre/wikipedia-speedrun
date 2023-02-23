aa99a9eaeb1445b518a597e075e77c36fc10e6ef
selligre <92374805+selligre@users.noreply.github.com>
2023-02-23 23:15:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import json
 BASE = 'https://iceandfire.fandom.com/wiki/'
 
 
-def main(source_str, cible_str):
+def main(source, cible):
     """doc main"""
-    print('source: ', source_str)
-    print('cible: ', cible_str)
+    print('source: ', source)
+    print('cible: ', cible)
 
 
 def liste_liens(page):
@@ -35,68 +35,68 @@ def liste_liens(page):
     return liste
 
 
-def svg_dico(dico, fichier):
-    with open(fichier, 'w') as f:
+def svg_dico(dico, file):
+    with open(file, 'w') as f:
         for key, value in dico.items():
             f.write(f'{key}: {value}\n')
 
 
-def svg_dico_json(dico, fichier):
-    with open(fichier, 'w') as f:
+def svg_dico_json(dico, file):
+    with open(file, 'w') as f:
         json_str = json.dumps(dico, indent=4)
         f.write(json_str)
 
 
-def chg_dico(fichier):
+def chg_dico(file):
     result = {}
-    with open(fichier, 'r') as f:
+    with open(file, 'r') as f:
         for line in f:
             key, value = line.strip().split(':')
             result[key] = value
     return result
 
 
-def chg_dico_json(fichier):
-    with open(fichier, 'r') as f:
+def chg_dico_json(file):
+    with open(file, 'r') as f:
         return json.load(f)
 
 
-def build_wiki_from(d, start_page):
-    # print(len(d.keys()))
-    if len(d.keys()) == 0:
-        d.update({start_page: liste_liens(start_page)})
-    for key in d.copy().keys():
-        if d.copy()[key] is not None:
-            for value in d.copy()[key]:
-                if value not in d.keys():
-                    d.update({value: liste_liens(value)})
+def build_wiki_from(dico, source):
+    print(len(dico.keys()))
+    if len(dico.keys()) == 0:
+        dico.update({source: liste_liens(source)})
+    for key in dico.copy().keys():
+        if dico.copy()[key] is not None:
+            for value in dico.copy()[key]:
+                if value not in dico.keys():
+                    dico.update({value: liste_liens(value)})
 
-    if not is_wiki_finished(d):
-        build_wiki_from(d, start_page)
-    return d
+    if not is_wiki_finished(dico):
+        build_wiki_from(dico, source)
+    return dico
 
 
 def build_wiki_from_largeur(wiki, source):
     f = []
-    fait = []
+    done = []
     f.append(source)
-    fait.append(source)
+    done.append(source)
     while f:
         print(len(wiki.keys()))
         source = f.pop(0)
         wiki.update({source: liste_liens(source)})
         for voisin in wiki[source]:
-            if voisin not in fait:
+            if voisin not in done:
                 f.append(voisin)
-                fait.append(voisin)
+                done.append(voisin)
 
 
-def is_wiki_finished(dico):
+def is_wiki_finished(wiki):
     result = True
-    for key in dico.copy().keys():
-        if dico.copy()[key] is not None:
-            for value in dico.copy()[key]:
-                if value not in dico.keys():
+    for key in wiki.copy().keys():
+        if wiki.copy()[key] is not None:
+            for value in wiki.copy()[key]:
+                if value not in wiki.keys():
                     result = False
     return result
 
@@ -109,26 +109,42 @@ def is_wiki_finished(dico):
 # temps d'exec pour build le wiki depuis 'Petyr_Baelish': 5min7sec, 2658 pages
 # temps d'exec pour build_largeur depuis 'Petyr_Baelish': 9min50sec, 2667 pages
 
-def plus_court_chemin(page_source, page_cible):
+def plus_court_chemin(source, cible):
+    # wiki to search
+    wiki = chg_dico_json('wiki.json')
     # result list
     result = []
-    result.append(page_source)
-    # wiki to search
-    wiki = build_wiki_from({}, page_source)
-    for key in wiki.keys():
+    found = False
+    result.append(source)
+    # go through wiki from source and not from the start
+    for key in wiki[source]:
         for value in wiki[key]:
-            if value == page_cible:
-                print(value)
-                print(key)
+            if value == cible:
                 result.append(key)
+                result.append(value)
+                found = True
+            if found:
                 break
-    result.append(page_cible)
+        if found:
+            break
+    if not found:
+        pass
     # RETURN
-    if not result[1]:
+    if not found:
+        print('no path found')
         return None
     return result
 
 
+# wiki = {}
+# wiki = build_wiki_from(wiki, 'Petyr_Baelish')
+# svg_dico_json(wiki, 'wiki.json')
+
 source = 'Dorne'
 cible = 'Rhaego'
-print(plus_court_chemin(source, cible))
+cible2 = 'Nymeria'
+cible3 = 'Arya_Stark'
+cible4 = 'Needle'
+print(plus_court_chemin(source, cible3))
+# ['Dorne', 'House_Targaryen', Rhaego']
+# ['Dorne', 'Rhoynar', 'Nymeria', 'Arya_Stark', 'Needle']
